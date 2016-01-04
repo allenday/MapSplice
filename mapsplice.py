44ea717154045360204b1259942496ffd280e007
Allen Day <no@sorry.com>
2016-01-03 21:47:44 -0500

    initialize spark

--- mapsplice.py
+++ mapsplice.py
@@ -21,6 +21,50 @@ import locale
 
 from datetime import datetime, date, time
 
+##########
+# initialize spark
+##########
+# Configure the necessary Spark environment
+import os
+import sys
+
+# Spark home
+spark_home = os.environ.get("SPARK_HOME")
+
+orig_sys_path = []
+for p in sys.path:
+  orig_sys_path.append(p)
+
+# If Spark V1.4.x is detected, then add ' pyspark-shell' to
+# the end of the 'PYSPARK_SUBMIT_ARGS' environment variable
+spark_release_file = spark_home + "/RELEASE"
+if os.path.exists(spark_release_file) and "Spark 1.4" in open(spark_release_file).read():
+    pyspark_submit_args = os.environ.get("PYSPARK_SUBMIT_ARGS", "")
+    if not "pyspark-shell" in pyspark_submit_args: pyspark_submit_args += " pyspark-shell"
+    os.environ["PYSPARK_SUBMIT_ARGS"] = pyspark_submit_args
+
+# Add the spark python sub-directory to the path
+sys.path.insert(0, spark_home + "/python")
+
+# Add the py4j to the path.
+# You may need to change the version number to match your install
+py4j_path = os.path.join(spark_home, "python/lib/py4j-0.8.2.1-src.zip")
+sys.path.insert(0, py4j_path)
+
+# Initialize PySpark to predefine the SparkContext variable 'sc'
+execfile(os.path.join(spark_home, "python/pyspark/shell.py"))
+
+while len(sys.path):
+  sys.path.pop()
+
+for p in orig_sys_path:
+  sys.path.append(p)
+
+##########
+# end initialize spark
+##########
+
+
 use_message = '''
 
 Usage:
